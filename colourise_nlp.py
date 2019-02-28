eb9ca03110c1025062c1bac5aab9c1688787965f
Will Holland <snowy.the.crow@gmail.com>
2019-02-28 07:50:39 +0000

    Add intermediate python interpretation
    Between nltk and rtf I've added a python object layer. This will make it easier
    to use this as an imported module and add new output formats

--- colourise_nlp.py
+++ colourise_nlp.py
@@ -1,12 +1,27 @@
 import fileinput
-import nltk
 import csv
+import sys
+import re
+
+import nltk
 
 
 RTF_HEADER = '''\
-{\\rtf1\\ansi\\deff0{\\fonttbl{\\f0 \\fswiss LiberationSans;}}
-{\\colortbl;\
+{\\rtf1\\ansi\\deff0{\\fonttbl{\\f0 \\fswiss LiberationSans;}}\
 '''
+
+
+class ColourisedWord(object):
+
+    def __init__(self, tagged_word, tag, whitespace, colour):
+        self.tagged_word = tagged_word
+        self.tag = tag
+        self.whitespace = whitespace
+        self.colour = colour
+
+    @property
+    def chars(self):
+        return self.tagged_word + self.whitespace
 
 
 class TaggingEngine(object):
@@ -18,24 +33,53 @@ class TaggingEngine(object):
 
     def init_rtf(self):
         print RTF_HEADER
-        for _, rgb in self.tags_colours_mapping:
-            print r'    \red{}\green{}\blue{};'.format(*rgb)
+        self._rtf_colours = list(set(t[1] for t in self.tags_colours_mapping))
+        sys.stdout.write(r'{\colortbl;')
+        for rgb in self._rtf_colours:
+            sys.stdout.write(r'\red{}\green{}\blue{};'.format(*rgb))
         print '}'
         print '\widowctrl\hyphauto'
 
-    def _get_colour_rtf(self, tag):
-        for i, (t, _) in enumerate(self.tags_colours_mapping):
+    def _get_colour_from_tag(self, tag):
+        for i, (t, c) in enumerate(self.tags_colours_mapping):
             if t == tag:
+                return c
+        return 0
+
+    def _get_colour_rtf(self, colour):
+        for i, c in enumerate(self._rtf_colours):
+            if c == colour:
                 return i + 1
         return 0
 
-    def tag_rtf(self, text):
+    def tag_python(self, text):
         tokens = nltk.word_tokenize(text)
-        tags = nltk.pos_tag(tokens) # [('test', 'NN')]
+        tags = nltk.pos_tag(tokens) # gives list like [('test', 'NN')]
+
+        # leading whitespace
+        match = re.match('^(\s*)', text)
+        if match:
+            whitespace = match.groups()[0]
+            colour = self._get_colour_from_tag('DEFAULT')
+            yield ColourisedWord('', 'DEFAULT', whitespace, colour)
+
+        # tokenize removes spaces, we still want to see them on the screen so add
+        # to the left
+        total = ''
+        text = text.lstrip()
+        for tagged_word, tag in tags:
+            match = re.match(re.escape(total)+re.escape(tagged_word)+'(\s*)', text)
+            whitespace = match.groups()[0]
+            total = total + tagged_word + whitespace
+            colour = self._get_colour_from_tag(tag)
+            yield ColourisedWord(tagged_word, tag, whitespace, colour)
+
+    def tag_rtf(self, text):
         result = ''
-        for word, tag in tags:
-            colour = self._get_colour_rtf(tag)
-            result = result + "{\\cf%s %s }" % (colour, word)
+        for word in self.tag_python(text):
+            c_index = self._get_colour_rtf(word.colour)
+            chars = word.chars.replace('\n', r'\line')
+            result = result + "{\\cf%s %s}" % (c_index, chars)
         return result
 
     def eof_rtf(self):
@@ -77,7 +121,6 @@ def main():
                 if c in '.?!':
                     print tagging_engine.tag(sentence)
                     sentence = ''
-            sentence += '\n'
     except KeyboardInterrupt:
         pass
     tagging_engine.eof()
